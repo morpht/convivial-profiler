eaa18a94d8b48b4ef8193ae7659744b429518ef7
Naveen Valecha <naveen.valecha@morpht.com>
2022-03-03 21:57:20 +0530

    convivial-profiler-26968: Detect mobile or desktop. (#2)

---
+++ src/processor_source/useragent.js
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * Convivial Profiler library source plugin.
+ *
+ * Copyright Morpht Pty Ltd 2020-2022.
+ */
+
+ (function (window, document) {
+  window.convivialProfiler = window.convivialProfiler || {};
+  window.convivialProfiler.processorSource = window.convivialProfiler.processorSource || {};
+  window.convivialProfiler.processorSource.useragent = function (processor, source, values) {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      values.push("mobile");
+    }
+    else {
+      values.push("browser");
+    }
+  }
+})(window, document);
