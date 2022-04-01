22ab5112ccb680a72da8e65bbf32632d74dc787d
Naveen Valecha <naveen.valecha@morpht.com>
2022-04-01 10:48:29 +0530

    convivial-profiler-27416: Provide the option of default_value to all the publishers. (#15)

--- src/processor_publisher/bestpick.js
+++ src/processor_publisher/bestpick.js
@@ -9,11 +9,6 @@
   window.convivialProfiler = window.convivialProfiler || {};
   window.convivialProfiler.processorPublisher = window.convivialProfiler.processorPublisher || {};
   window.convivialProfiler.processorPublisher.bestpick = function (publisher, values) {
-    // Remove empty and null values.
-    values = values.filter(el => {return el != null && el != '';});
-    if (Array.isArray(values) && values.length === 0 && publisher.default_value && publisher.default_value !== undefined) {
-      values.push(publisher.default_value);
-    }
     // Store the value in localstorage if its applicable.
     if (publisher.storage.localstorage === 'localstorage' && values[0]) {
       localStorage.setItem(publisher.key, values[0]);
