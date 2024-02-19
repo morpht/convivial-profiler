96e0ae4229a9a66db274e78d356ecc52b161c377
Naveen Valecha <naveen.valecha@morpht.com>
2024-02-19 12:12:17 +0530

    Issue-58: Handle error gracefully when local storage is disabled.

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,6 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      require("cypress-localstorage-commands/plugin")(on, config);
-      return config;
     },
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.js',
